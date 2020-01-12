1376c51569db7c2f818cdf05d21199120c2a44a9
MelissaDTH <melissa.hart@ziggo.nl>
2020-01-12 18:02:34 +0100

    added seeding data

--- seeds.js
+++ seeds.js
@@ -43,98 +43,128 @@ db.sync({ force: true })
       // CATEGORIES
       Category.create({
         name: "Home",
-        description: "Placeholder text to see if it gets rendered",
+        description: "When it comes to home improvement, look no further than the home and garden pages of Marketplace. A wide range of home goods are just a mouse click away.",
         picture:
           "https://images.pexels.com/photos/584399/living-room-couch-interior-room-584399.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
       }),
       Category.create({
         name: "Electronics",
-        description: "Placeholder text to see if it gets rendered",
+        description: "Keep up with the latest in electronics and technology and save money with great deals. Shop with confidence and find the electronics you want for a great price on Marketplace.",
         picture:
           "https://images.pexels.com/photos/35550/ipad-tablet-technology-touch.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
       }),
       Category.create({
         name: "Plants and Gardening",
-        description: "Placeholder text to see if it gets rendered",
+        description: "Plants are a great way to make your house a better space to live in. You’ll find everything you need in and around your home to make it uniquely your own.",
         picture:
           "https://images.pexels.com/photos/1600130/pexels-photo-1600130.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
       }),
       Category.create({
         name: "Fashion",
-        description: "Placeholder text to see if it gets rendered",
+        description: "Take the strain out of shopping with Marketplace. Find great deals on fashionable jeans, sweaters, suits and that look sharp and stylish.",
         picture:
           "https://images.pexels.com/photos/794064/pexels-photo-794064.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
       }),
       Category.create({
         name: "Sporting Goods",
-        description: "Placeholder text to see if it gets rendered",
+        description: "Are you ready to play like a pro? No matter what your favorite sport is, Marketplace has the equipment you need to perform at your peak.",
         picture:
           "https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
       }),
       Category.create({
         name: "Movies, music and games",
-        description: "Placeholder text to see if it gets rendered",
+        description: "Turn your home into a theater-like experience with a new or refurbished TV by Samsung, LG, or Sharp. Or up your game with a new or refurbished video game console like the PS4 or Xbox One.",
         picture:
           "https://images.pexels.com/photos/1040160/pexels-photo-1040160.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
       }),
       // PRODUCTS
       Products.create({
-        name: "Dinner table",
+        name: "Brown Wooden Dinner Table with Ceramic Plate",
         picture:
-          "https://picsum.photos/536/354",
-          price: 199,
-        description: "Product text",
+          "https://images.pexels.com/photos/154161/pexels-photo-154161.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+          price: 399,
+        description: "Selling a nearly perfect table with a ceramic plate, I put a bowl with lettuce on it just for show. Not selling this plate!",
         color: 'brown',
         userId: 1,
         categoryId: 1
       }),
       Products.create({
-        name: "Lamp",
+        name: "White Lamp with black Lamp Base",
         picture:
-          "https://picsum.photos/536/354",
+          "https://images.pexels.com/photos/707579/pexels-photo-707579.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
           price: 54,
-        description: "Product text lamp",
+        description: "A white lamp that has to go because we have bought new black lamps. This does not fit our interior anymore, they are almost new and have been use scarcely.",
         color: 'black',
         userId: 2,
         categoryId: 1
       }),
       Products.create({
-        name: "iPhone",
+        name: "New Silver iPhone X With Airpods",
         picture:
-          "https://picsum.photos/536/354",
+          "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
           price: 850,
-        description: "Product text iphone",
+        description: "A silver iPhone X, I want to buy the newer version that came out last month. I've had this one for three months, so it's very new. It has been in the case since purchase, so it is practically unused.",
         color: 'grey',
         userId: 3,
         categoryId: 2
       }),
       Products.create({
-        name: "VR headset",
+        name: "PSVR (Playstation Virtual Reality) Headset",
         picture:
-          "https://picsum.photos/536/354",
-          price: 850,
-        description: "Product text of PSVR",
+          "https://images.pexels.com/photos/3391378/pexels-photo-3391378.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+          price: 250,
+        description: "My son's Playstation VR Headset has to go. He barely has time for his family or his homework, we gave him an ultimatum and eventually he decided to keep living with us and to sell the headset",
         color: 'white',
         userId: 2,
         categoryId: 2
       }),
       Products.create({
-        name: "Succulent",
+        name: "iMac 21'' Slim (2019)",
         picture:
-          "https://picsum.photos/536/354",
+          "https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+          price: 1150,
+        description: "I found out that this programming thing that everyone is talking about and doing bootcamps for,  is not my thing. All the debugging and learning, I don't understand why anyone would be able to do this. So, I'm selling my iMac.",
+        color: 'white',
+        userId: 5,
+        categoryId: 2
+      }),
+      Products.create({
+        name: "Sony PSP Playstation Handheld",
+        picture:
+          "https://images.pexels.com/photos/1435595/pexels-photo-1435595.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+          price: 350,
+        description: "There have been no new games for the Sony PSP that cam out that I liked. Actually, are they still made because I couldn't find any the last couple of years. It's still a great handheld to keep in your hand though, so it's a great purchase.",
+        color: 'black',
+        userId: 4,
+        categoryId: 2
+      }),
+      Products.create({
+        name: "Two Playstation 4 Dualshock Controllers",
+        picture:
+          "https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+          price: 250,
+        description: "The Playstation 5 will be coming to the market in the end of year 2020, so I'm already selling all my Playstation 4 stuff (and FIFA20 as well, I hate all the rage quiting it makes me do) to prepare for the PS5 release.",
+        color: 'black',
+        userId: 2,
+        categoryId: 2
+      }),
+      Products.create({
+        name: "Succulent Plant Trio for Indoor",
+        picture:
+          "https://images.pexels.com/photos/796620/pexels-photo-796620.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
           price: 15,
-        description: "Product text plants",
+        description: "These indoor succulents come with three plants, without any pots. They are only suitable for indoor life.",
         color: 'green',
         userId: 5,
         categoryId: 3
       }),
       Products.create({
-        name: "Pot",
+        name: "Orange 25 cm Indoor Plant Pot",
         picture:
-          "https://picsum.photos/536/354",
+          "https://images.pexels.com/photos/1533960/pexels-photo-1533960.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
           price: 10,
-        description: "Product text of the pot",
-        color: 'yellow',
+        description: "A new pot for small sized plants. Has never been used, this purchase will be without the plant.",
+        color: 'orange',
         userId: 4,
         categoryId: 3
       })
